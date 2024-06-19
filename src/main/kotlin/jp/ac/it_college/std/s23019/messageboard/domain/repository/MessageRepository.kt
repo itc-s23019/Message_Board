0cd52604b6c034677e5bb5bf23ca271da022e3e0
itc-s23019 <s23019@std.it-college.ac.jp>
2024-06-19 16:09:13 +0900

    domain/MessageRepositoryを記入

---
+++ src/main/kotlin/jp/ac/it_college/std/s23019/messageboard/domain/repository/MessageRepository.kt
@@ -0,0 +1,17 @@
+package jp.ac.it_college.std.s23019.messageboard.domain.repository
+
+import jp.ac.it_college.std.s23019.messageboard.domain.model.Messages
+
+
+interface MessageRepository {
+    fun createMessage(message: Messages): Messages
+
+    fun getMessageId(id: Long): Messages?
+
+    fun getMessageByThreadId(threadId: Long): List<Messages>
+
+    fun updateMessage(message: Messages): Messages
+
+    fun deleteMessage(id: Long)
+
+}
