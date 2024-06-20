b4af4ae34877bec00b281d4e46d627264f26ef7f
itc-s23019 <s23019@std.it-college.ac.jp>
2024-06-20 14:19:16 +0900

    MessageServiceを記入

---
+++ src/main/kotlin/jp/ac/it_college/std/s23019/messageboard/application/service/MessageService.kt
@@ -0,0 +1,15 @@
+package jp.ac.it_college.std.s23019.messageboard.application.service
+
+import jp.ac.it_college.std.s23019.messageboard.domain.model.Messages
+import jp.ac.it_college.std.s23019.messageboard.domain.repository.MessageRepository
+import org.springframework.stereotype.Service
+
+
+@Service
+class MessageService(
+    private val messageRepository: MessageRepository
+) {
+    fun createMessage(message: Messages): Messages {
+        return messageRepository.createMessage(message)
+    }
+}
